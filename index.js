0b337593327ea976f54df9cd5fcd294ac3a244f1
golfstrimmar <yushinbox@gmail.com>
2024-11-02 20:33:35 +0100

    20.33

---
+++ index.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+// let res = fs.readFile('some.txt', 'utf-8', (err, data) => {
+//   fs.writeFile('some.txt', data + 'Hello!', (err, data) => {
+//     console.log('ok')
+//   })
+// })
+// fs.mkdir('text-files', () => {
+//   fs.writeFile('./text-files/some.txt', 'Hallo!', () => {
+//   })
+// })
+// fs.unlink('./text-files/some.txt', () => {
+//   fs.rmdir('./text-files', () => {
+//   })
+// })
+// fs.unlink('./some.txt', () => {
+// })
