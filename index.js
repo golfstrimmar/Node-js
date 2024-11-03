9ea3298cb5167219761d112a16f5a16a98e9c452
golfstrimmar <yushinbox@gmail.com>
2024-11-03 11:24:03 +0100

    20.33

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const fs = require('fs')
-// let res = fs.readFile('some.txt', 'utf-8', (err, data) => {
-//   fs.writeFile('some.txt', data + 'Hello!', (err, data) => {
-//     console.log('ok')
-//   })
-// })
+// let res = fs.readFile("some.txt", "utf-8", (err, data) => {
+//   fs.writeFile("some.txt", data + "Hello!", (err, data) => {
+//     console.log("ok");
+//   });
+// });
 // fs.mkdir('text-files', () => {
 //   fs.writeFile('./text-files/some.txt', 'Hallo!', () => {
 //   })
@@ -12,5 +11,36 @@ const fs = require('fs')
 //   fs.rmdir('./text-files', () => {
 //   })
 // })
-// fs.unlink('./some.txt', () => {
-// })
+// fs.unlink("./some.txt", () => {});
+// res.end(
+//   "<!doctype html>\n" +
+//     '<html lang="en">\n' +
+//     "<head>\n" +
+//     '    <meta charset="UTF-8">\n' +
+//     '    <meta name="viewport"\n' +
+//     '          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">\n' +
+//     '    <meta http-equiv="X-UA-Compatible" content="ie=edge">\n' +
+//     "    <title>Node-js</title>\n" +
+//     "</head>\n" +
+//     "<body>\n" +
+//     "<h1>\n" +
+//     "    Hallo node!\n" +
+//     "</h1>\n" +
+//     "</body>\n" +
+//     "</html>",
+// );
+// const stream = fs.createReadStream("./templates/index.html");
+// stream.pipe(res);
+const fs = require("fs");
+const http = require("http");
+let server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+  if (req.url == "/") fs.createReadStream("./templates/index.html").pipe(res);
+  else if (req.url == "/about")
+    fs.createReadStream("./templates/about.html").pipe(res);
+});
+const PORT = 3000;
+const HOST = "localhost";
+server.listen(PORT, HOST, () => {
+  console.log(`server: http://${HOST}:${PORT}`);
+});
